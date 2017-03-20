3679bc9a4ef1f87bdf0c83762b4a8af4fd8caea7
Trent Hand <livefree1983@gmail.com>
2017-03-20 09:23:34 -0500

    Image can be selected from the photo library.  This updates the image in the add screen

--- BookViewController.swift
+++ BookViewController.swift
@@ -8,20 +8,41 @@
 
 import UIKit
 
-class BookViewController: UIViewController {
+class BookViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     @IBOutlet weak var bookImageView: UIImageView!
     @IBOutlet weak var titleTextField: UITextField!
+    
+//    let's the user select something from the library or take a new photo'
+    var imagePicker = UIImagePickerController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        imagePicker.delegate = self
+        
     }
 
     @IBAction func addTapped(_ sender: Any) {
     }
+    
     @IBAction func photosTapped(_ sender: Any) {
+//        this is how you get pictures off of a user's phone'
+        imagePicker.sourceType = .photoLibrary
+        
+        present(imagePicker, animated: true, completion: nil)
     }
+//    function allowing the user to select the image and add it to a new book
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        let image = info[UIImagePickerControllerOriginalImage] as! UIImage
+        
+        bookImageView.image = image
+//        we must dismiss the previous image to make this work
+        imagePicker.dismiss(animated: true, completion: nil)
+        
+    }
+    
 
     @IBAction func cameraTapped(_ sender: Any) {
     }
